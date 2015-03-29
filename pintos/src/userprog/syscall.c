c3d349b81da5eab0a0316b3d80d7d21821520e50
Kevin Chen <kvchen@berkeley.edu>
2015-03-29 01:45:56 -0700

    Implement halt syscall and improve name printing in exit

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <syscall-nr.h>
+#include "devices/shutdown.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
@@ -16,7 +17,9 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  syscall_list[SYS_HALT] = syscall_halt;
   syscall_list[SYS_EXIT] = syscall_exit;
+  syscall_list[SYS_EXEC] = syscall_exec;
   syscall_list[SYS_WRITE] = syscall_write;
   syscall_list[SYS_NULL] = syscall_null;
 }
@@ -28,28 +31,36 @@ syscall_handler (struct intr_frame *f UNUSED)
   syscall_list[args[0]](args, &f->eax);
 }
 
+
+/* Syscall implementations. */
+
 void
-syscall_exit (uint32_t *args, uint32_t *eax)
+syscall_halt (uint32_t *args UNUSED, uint32_t *retval UNUSED)
 {
-    char procname[15];
+    shutdown_power_off ();
+}
 
-    /* Get the process name without arguments. */
+void
+syscall_exit (uint32_t *args, uint32_t *retval)
+{
+    /* Print the process name without arguments. */
     struct thread *t = thread_current ();
-    strlcpy (procname, t->name, 15);
+    char *name_end = strchr(t->name, ' ');
 
-    char *pn_end = strchr(procname, ' ');
-    if (pn_end) {
-        *pn_end = '\0';
-    }
-
-    printf("%s: exit(%d)\n", procname, args[1]);
+    printf("%.*s: exit(%d)\n", name_end-t->name, t->name, args[1]);
     
-    *eax = args[1];
+    *retval = args[1];
     thread_exit();
 }
 
 void
-syscall_write (uint32_t *args, uint32_t *eax)
+syscall_exec (uint32_t *args, uint32_t *retval)
+{
+    const char *cmd_line = (char *) args[1];
+}
+
+void
+syscall_write (uint32_t *args, uint32_t *retval)
 {
     int fd = (int) args[1];
     const char *buffer = (char *) args[2];
@@ -62,13 +73,13 @@ syscall_write (uint32_t *args, uint32_t *eax)
         printf("%c", *(buffer+written));
     }
 
-    *eax = written;
+    *retval = written;
 }
 
 void
-syscall_null(uint32_t *args, uint32_t *eax)
+syscall_null(uint32_t *args, uint32_t *retval)
 {
-    *eax = args[1] + 1;
+    *retval = args[1] + 1;
 }
 
 
--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -5,10 +5,12 @@
 
 void syscall_init (void);
 
-typedef void (*syscall_fun_t) (uint32_t *args, uint32_t *eax);
+typedef void (*syscall_fun_t) (uint32_t *args, uint32_t *retval);
 
-void syscall_exit (uint32_t *args, uint32_t *eax);
-void syscall_write (uint32_t *args, uint32_t *eax);
-void syscall_null (uint32_t *args, uint32_t *eax);
+void syscall_halt (uint32_t *args, uint32_t *retval);
+void syscall_exit (uint32_t *args, uint32_t *retval);
+void syscall_exec (uint32_t *args, uint32_t *retval);
+void syscall_write (uint32_t *args, uint32_t *retval);
+void syscall_null (uint32_t *args, uint32_t *retval);
 
 #endif /* userprog/syscall.h */
