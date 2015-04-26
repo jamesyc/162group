8b1112ebe914b8fb593595d115b4b400cb649f4e
Kevin Chen <kvchen@berkeley.edu>
2015-04-25 23:57:55 -0700

    Add hash table for faster key lookups, fixes #22

--- kvstore/src/server/kvcacheset.c
+++ kvstore/src/server/kvcacheset.c
@@ -23,6 +23,7 @@ int kvcacheset_init(kvcacheset_t *cacheset, unsigned int elem_per_set) {
   
   cacheset->num_entries = 0;
   cacheset->entries = NULL;
+  cacheset->hash = NULL;
   return 0;
 }
 
@@ -33,15 +34,14 @@ int kvcacheset_init(kvcacheset_t *cacheset, unsigned int elem_per_set) {
 int kvcacheset_get(kvcacheset_t *cacheset, char *key, char **value) {
   struct kvcacheentry *elt;
 
-  DL_FOREACH(cacheset->entries, elt) {
-    if (!strcmp(elt->key, key)) {
-      *value = malloc(strlen(elt->value)+1);
-      strcpy(*value, elt->value);
-      return 0;
-    }
-  }
+  HASH_FIND_STR(cacheset->hash, key, elt);
 
-  return ERRNOKEY;
+  if (!elt)
+    return ERRNOKEY;
+
+  *value = malloc(strlen(elt->value)+1);
+  strcpy(*value, elt->value);
+  return 0;
 }
 
 /* Add the given KEY, VALUE pair to CACHESET. Returns 0 if successful, else
@@ -49,19 +49,17 @@ int kvcacheset_get(kvcacheset_t *cacheset, char *key, char **value) {
  * exceed CACHESET->elem_per_set total entries. */
 int kvcacheset_put(kvcacheset_t *cacheset, char *key, char *value) {
   struct kvcacheentry *new_entry;
-  struct kvcacheentry *el, *tmp;
+  struct kvcacheentry *elt;
 
   /* Replace an old entry if overwritten. */
-  DL_FOREACH_SAFE(cacheset->entries, el, tmp) {
-    if (!strcmp(el->key, key)) {
-      el->value = value;
-      return 0;
-    }
+  HASH_FIND_STR(cacheset->hash, key, elt);
+  if (elt) {
+    elt->value = value;
+    return 0;
   }
 
   new_entry = malloc(sizeof(struct kvcacheentry));
-
-  if (new_entry == NULL) {
+  if (!new_entry) {
     return ERRFILLEN;
   }
 
@@ -73,18 +71,21 @@ int kvcacheset_put(kvcacheset_t *cacheset, char *key, char *value) {
   if (cacheset->num_entries < cacheset->elem_per_set) {
     cacheset->num_entries++;
   } else {
-    DL_FOREACH(cacheset->entries, el) {
-      DL_DELETE(cacheset->entries, el);
+    DL_FOREACH(cacheset->entries, elt) {
+      DL_DELETE(cacheset->entries, elt);
 
-      if (!el->refbit)
+      if (!elt->refbit)
+        HASH_DEL(cacheset->hash, elt);
+        free(elt);
         break;
 
-      el->refbit = false;
-      DL_APPEND(cacheset->entries, el);
+      elt->refbit = false;
+      DL_APPEND(cacheset->entries, elt);
     }
   }
 
   DL_APPEND(cacheset->entries, new_entry);
+  HASH_ADD_KEYPTR(hh, cacheset->hash, key, strlen(key), new_entry);
   return 0;
 }
 
@@ -93,11 +94,12 @@ int kvcacheset_put(kvcacheset_t *cacheset, char *key, char *value) {
 int kvcacheset_del(kvcacheset_t *cacheset, char *key) {
   struct kvcacheentry *elt;
 
-  DL_FOREACH(cacheset->entries, elt) {
-    if (!strcmp(elt->key, key)) {
-      DL_DELETE(cacheset->entries, elt);
-      return 0;
-    }
+  HASH_FIND_STR(cacheset->hash, key, elt);
+  if (elt) {
+    HASH_DEL(cacheset->hash, elt);
+    DL_DELETE(cacheset->entries, elt);
+    free(elt);
+    return 0;
   }
 
   return ERRNOKEY;
@@ -107,8 +109,10 @@ int kvcacheset_del(kvcacheset_t *cacheset, char *key) {
 void kvcacheset_clear(kvcacheset_t *cacheset) {
   struct kvcacheentry *elt, *tmp;
 
-  DL_FOREACH_SAFE(cacheset->entries, elt, tmp) {
+  HASH_ITER(hh, cacheset->hash, elt, tmp) {
+    HASH_DEL(cacheset->hash, elt);
     DL_DELETE(cacheset->entries, elt);
+    free(elt);
   }
 }
 
