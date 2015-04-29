96a9fdf2268ab11457e5c7c2f9b29bb3462dcd1d
Kevin Chen <kvchen@berkeley.edu>
2015-04-28 21:44:48 -0700

    Change switch statement to if/elif block to suppress gcc warning

--- kvstore/src/server/kvserver.c
+++ kvstore/src/server/kvserver.c
@@ -171,23 +171,17 @@ void kvserver_handle_no_tpc(kvserver_t *server, kvmessage_t *reqmsg,
   /* Set default response type. */
   respmsg->type = RESP;
 
-  switch(reqmsg->type) {
-    case GETREQ:
-      error = kvserver_get(server, reqmsg->key, value);
-
-      if (!error) {
-        respmsg->type = GETRESP;
-        respmsg->key = reqmsg->key;
-        respmsg->value = *value;
-      }
-
-      break;
-    case PUTREQ:
-      error = kvserver_put(server, reqmsg->key, reqmsg->value);
-      break;
-    case DELREQ:
-      error = kvserver_del(server, reqmsg->key);
-      break;
+  if (reqmsg->type == GETREQ) {
+    error = kvserver_get(server, reqmsg->key, value);
+    if (!error) {
+      respmsg->type = GETRESP;
+      respmsg->key = reqmsg->key;
+      respmsg->value = *value;
+    }
+  } else if (reqmsg->type == PUTREQ) {
+    error = kvserver_put(server, reqmsg->key, reqmsg->value);
+  } else if (reqmsg->type == DELREQ) {
+    error = kvserver_del(server, reqmsg->key);
   }
 
   if (!error) {
